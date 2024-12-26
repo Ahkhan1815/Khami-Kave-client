65c2b7023a44f7f15d36a95d786c3fa9c28c5085
Ahkhan1815 <ahkhan1815@gmail.com>
2024-12-26 12:02:59 -0600

    error handling

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -51,16 +51,28 @@ function Post() {
     const deletePost = (id) => {
         console.log(id);
         axios.delete(`https://khami-kave-server.onrender.com/gameposts/byId/${id}`).then((response) => {
+            if(response.data.error){
+                alert(response.data.error);
+            }
+            else{
+                console.log("Successfully deleted post");
+            }
             navigate("/");
-        })
+        });
     }
 
     const deleteComment = (id) => {
         console.log(id);
         console.log("test");
         axios.delete(`https://khami-kave-server.onrender.com/comments/${id}`).then((response) => {
+            if(response.data.error){
+                alert(response.data.error);
+            }
+            else{
+                console.log("Successfully deleted post");
+            }
             navigate(0);
-        })
+        });
     }
     const initialValues = {
         commentBody: ""
