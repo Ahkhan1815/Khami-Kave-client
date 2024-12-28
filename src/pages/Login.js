68b19bbb8958da8d10d5d5aee5e5eeeb089be7f7
Ahkhan1815 <ahkhan1815@gmail.com>
2024-12-27 22:36:35 -0600

    Added Loading Messages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,39 +8,66 @@ const { backendURL } = require('../components/constants');
 function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
 
     let navigate = useNavigate();
 
 
-    const login = () => {
-        const data = { username: username, password: password };
-        axios.post(backendURL +"/auth/login", data).then((response) => {
-            if (response.data.error) alert(response.data.error);
+    const login = async () => {
+        setLoading(true);
+        try {
+            const data = { username: username, password: password };
+            const response = await axios.post(backendURL + "/auth/login", data);
+            if (response.data.error) {
+                alert(response.data.error);
+            }
             else {
                 localStorage.setItem("accessToken", response.data);
                 navigate("/");
             }
-        })
+        }
+        catch (error) {
+            console.log(error);
+            alert("There was an error logging in, please try again.");
+        }
+        finally {
+            setLoading(false);
+        }
     };
-    return (
 
-        <div class="container d-flex justify-content-center">
+    if (loading) {
+        return <div class="container d-flex justify-content-center">
             <div className="card col-12 col-md-8 col-lg-6">
                 <div className="card-header text-center fs-5">
                     Login
                 </div>
                 <div className="card-body">
-                    <input type="text" placeholder='Username...'  className = "form-control mb-2 border-primary" onChange={(event) => {
+                Logging in...Please Wait
+                </div>
+            </div>
+        </div>
+    }
+
+    return (
+
+        <div class="container d-flex justify-content-center">
+
+            <div className="card col-12 col-md-8 col-lg-6">
+                <div className="card-header text-center fs-5">
+                    Login
+                </div>
+                <div className="card-body">
+                    <input type="text" placeholder='Username...' className="form-control mb-2 border-primary" onChange={(event) => {
                         setUsername(event.target.value);
                     }}
                     />
-                    <input type="password" placeholder= "Password..." className = "form-control border-primary"  onChange={(event) => {
+                    <input type="password" placeholder="Password..." className="form-control border-primary" onChange={(event) => {
                         setPassword(event.target.value);
                     }}
                     />
                 </div>
                 <div className="card-footer text-center">
-                    <button class = "btn btn-outline-warning" onClick={login}> Login </button>
+                    <button class="btn btn-outline-warning" onClick={login}> Login </button>
                 </div>
             </div>
         </div>
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -5,6 +5,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from 'react-router-dom';
 import { Filter } from 'glin-profanity';
+import { useState } from "react";
 
 const { backendURL } = require('../components/constants');
 const profanityCheck = new Filter();
@@ -15,54 +16,78 @@ function Registration() {
         password: "",
         email: ""
     };
+    const [loading, setLoading] = useState(false);
 
     let navigate = useNavigate();
 
     const validationSchema = Yup.object().shape({
         username: Yup.string().min(4, "Username is too short, minimum length is 4 characters").max(15, "Username is too long, maximum length is 15 characters").required("Username is required").test('no-spaces', 'No spaces allowed', function (value) {
             return !/\s/.test(value);
-          }).test("profanityCheck", "This title contains profanity", (value) => !profanityCheck.isProfane(value)),
+        }).test("profanityCheck", "This title contains profanity", (value) => !profanityCheck.isProfane(value)),
         password: Yup.string().min(6, "Password is too short, minimum length is 6 characters").max(20, "Password is too long, maximum length is 20 characters").required("Password is required"),
         email: Yup.string().required("Email is required"),
     });
 
-    const onSubmit = (data) => {
-        axios.post(backendURL + "/auth", data).then((response)=> {
+    const onSubmit = async (data) => {
+        setLoading(true);
+        try {
+            const response = await axios.post(backendURL + "/auth", data);
             if (response.data.error) {
                 alert(response.data.error);
-            } else {
+            }
+            else {
                 console.log(data);
                 navigate("/login");
             }
-        })
+        }
+        catch (error) {
+            console.log(error);
+            alert("There was an error during registration, please try again.");
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    if (loading) {
+        return <div class="container d-flex justify-content-center">
+            <div className="card col-12 col-md-8 col-lg-6">
+                <div className="card-header text-center fs-5">
+                    Create an Account
+                </div>
+                <div className="card-body">
+                    Registering Account... When this page redirects, check your email to verify your account. Make sure to check your spam folder too!
+                </div>
+            </div>
+        </div>
     }
 
     return (
-        <div class= "container d-flex justify-content-center">
-            <div className = "card col-12 col-md-8 col-lg-6">
-            <div className= "card-header text-center fs-5">
-                Create an Account
-            </div>
-            <div className= "card-body">
-            
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-                <Form className="form-container">
+        <div class="container d-flex justify-content-center">
+            <div className="card col-12 col-md-8 col-lg-6">
+                <div className="card-header text-center fs-5">
+                    Create an Account
+                </div>
+                <div className="card-body">
 
-                    <Field class= "border border-primary mb-3" id="post-field" name="username" placeholder="(Username...)" />
-                    <Field class= "border border-primary mb-3" id="post-field"  type= "password" name="password" placeholder="(Your Password...)" />
-                    <Field class= "border border-primary mb-3" id="post-field" name="email" placeholder="(Email...)" />
-                    <ErrorMessage name="username" component="span" />
-                    <ErrorMessage name="password" component="span" />
-                    <ErrorMessage name="email" component="span" />
-                    <div className="button-container">
-                        <button class = "btn btn-outline-warning" type="submit">Sign up!</button>
-                    </div>
-                </Form>
-            </Formik>
-            </div>
-            <div className= "card-footer text-muted fs-6">
-                Verify your account via email to post and comment. 
-            </div>
+                    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                        <Form className="form-container">
+
+                            <Field class="border border-primary mb-3" id="post-field" name="username" placeholder="(Username...)" />
+                            <Field class="border border-primary mb-3" id="post-field" type="password" name="password" placeholder="(Your Password...)" />
+                            <Field class="border border-primary mb-3" id="post-field" name="email" placeholder="(Email...)" />
+                            <ErrorMessage name="username" component="span" />
+                            <ErrorMessage name="password" component="span" />
+                            <ErrorMessage name="email" component="span" />
+                            <div className="button-container">
+                                <button class="btn btn-outline-warning" type="submit">Sign up!</button>
+                            </div>
+                        </Form>
+                    </Formik>
+                </div>
+                <div className="card-footer text-muted fs-6">
+                    Verify your account via email to post and comment. Make sure to check your spam folder too!
+                </div>
             </div>
         </div>
     )
